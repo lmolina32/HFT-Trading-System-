5028b85bc39e12935f0bbac622be947c5290e3f0
Leonardo Molina <lmolina3@nd.edu>
2026-02-18 01:07:49 +0000

    feat: skeleton + idea on order_entry.py

---
+++ order_entry.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+"""
+order_entry.py: TCP connection to exchange to send orders
+"""
+
+import socket
+import sys
+
+
+
+class OrderEntryClient:
+    def __init__(self):
+        pass
+
+    def log_in(self):
+        pass
+
+    def parse_response(self):
+        pass
+
+    def recv_response(self):
+        pass
+
+    def handle_order_error(self):
+        pass
+
+    def handle_log_in_error(self):
+        pass
+
+    def send_order(self):
+        pass
+
+    def new_order(self):
+        pass
+
+    def delete_order(self):
+        pass
+
+    def modify_order(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
